5ea4a946cf50b7f8cede5c04e4c274495b4c7db9
Rolazar Aberin <rolazaraberin@gmail.com>
2022-05-05 23:48:52 -0700

    Added a scoring system

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 $("#startButton").click(start);
+$("#solveButton").click(solve);
 $("#letterSection button").click(pressedLetter);
 $(document).keypress(pressedKey);
 
@@ -9,7 +10,8 @@ var quote;
 var author;
 var guessCount;
 var bestGuessCount;
-//var unguessedLetters;
+var score;
+var scoreMultiplier = 100;
 
 /************************************************
  * FUNCTIONS
@@ -17,11 +19,49 @@ var bestGuessCount;
 async function start() {
   showLoadingScreen();
   await getQuote();
+  hideStartButton();
+  showSolveButton();
   closeLoadingScreen();
   createBlanks();
-  showKeyboard();
-}
+  setScore();
+  showScore();
+  showQuoteSection();
+  showLetterSection();
 
+  /**************************************
+   * HELPER FUNCTIONS
+   **************************************/
+  function hideStartButton() {
+    $("#startButton").addClass("d-none");
+  }
+  function showSolveButton() {
+    $("#solveButton").removeClass("d-none");
+  }
+  function showQuoteSection() {
+    $("#quoteSection").removeClass("d-none");
+  }
+  function showScore() {
+    $("#scoreSection").removeClass("d-none");
+  }
+}
+function setScore() {
+  score = numberOfLetters(quote) * scoreMultiplier;
+  $("#score").text(score);
+}
+function decrementScore() {
+  score -= scoreMultiplier;
+  $("#score").text(score);
+}
+function numberOfLetters(string) {
+  let letterCount = 0;
+  for (let character of string) {
+    if (isLetter(character)) letterCount++;
+  }
+  return letterCount;
+}
+function solve() {
+  console.log("clicked solve");
+}
 async function showLoadingScreen() {
   console.log("loading...");
 }
@@ -40,54 +80,75 @@ async function getQuote() {
   author = response.author;
 }
 function createBlanks() {
-  let character;
-  let word = document.createElement("div");
-  let $word = $(word);
-  for (character of quote) {
+  let $word = $(startNewWord());
+  for (let character of quote) {
     //console.log(letter);
-    let blank = document.createElement("div");
-    let $blank = $(blank);
-    $blank.addClass("col btn");
+    //let $blank = createLetterBlank(character);
     if (isLetter(character)) {
       //console.log("character is a letter");
-      $blank.attr("value", character);
-      $blank.text("?");
-      $blank.addClass("btn-secondary blankLetter");
+      let $blank = createLetterBlank(character);
+      // $blank.attr("value", character);
+      // $blank.text("?");
+      // $blank.addClass("btn-secondary blankLetter");
       $word.append($blank);
     } else {
       //console.log("character is a punctuation");
       //if (character == " ") character = "_";
       $("#quote").append($word);
-      if (character != " ") {
-        $blank.text(character);
-        $("#quote").append($blank);
-      }
-      word = document.createElement("div");
-      $word = $(word);
+      $("#quote").append(character);
+      // if (character != " ") {
+      //   $blank.text(character);
+      //   $("#quote").append($blank);
+      // }
+      //word = document.createElement("div");
+      //$word = $(word);
+      $word = $(startNewWord());
     }
   }
-  if (isLetter(character)) {
-    //Quote is missing final punctuation
-    console.log("last character is a letter");
+  if (isWordUnresolved($word)) {
+    console.log("Quote is missing final punctuation");
     $("#quote").append($word);
   }
   $("#author").html(author);
+
+  /****************************************
+   * HELPER FUNCTIONS
+   ****************************************/
+  function startNewWord() {
+    let word = document.createElement("div");
+    return word;
+  }
+  function isWordUnresolved($wordElement) {
+    //console.log("text", $wordElement.text() == "");
+    return $wordElement.text() != "";
+  }
+  function createLetterBlank(letter) {
+    let blank = document.createElement("div");
+    let $blank = $(blank);
+    $blank.addClass("col btn btn-secondary blankLetter");
+    $blank.attr("value", letter);
+    $blank.text("?");
+    return $blank;
+  }
 }
-function isLetter(char) {
+function isLetter(character) {
   //console.log("checking if letter");
-  return char.match(/[a-z]/i);
+  return character.match(/[a-z]/i);
 }
 function revealQuote() {
   console.log("revealing quote");
 }
-function showKeyboard() {
+function showLetterSection() {
   $("#letterSection").removeClass("d-none");
 }
 function guessedLetter(letter) {
-  incrementGuessCount();
+  //incrementGuessCount();
+  decrementScore();
   disableLetter(letter);
   let isLetterFound = revealLettersInQuote(letter);
-  if (!isLetterFound) {
+  if (isLetterFound) {
+    outputMessage("Good guess!", "flash", "green");
+  } else {
     outputMessage("Guess again", "flash", "red");
     //console.log("letter not in quote");
   }
@@ -95,28 +156,16 @@ function guessedLetter(letter) {
   /*******************************************
    * HELPER FUNCITONS
    *******************************************/
-  function incrementGuessCount() {
+  /*function incrementGuessCount() {
     let $guess = $("#guessCount");
     let guessCount = $guess.text();
     guessCount++;
     $guess.text(guessCount);
-  }
+  }*/
   function disableLetter(letter) {
     let $letterElement = $(`#letterSection button:contains(${letter})`);
     $letterElement.attr("disabled", true);
   }
-  /*
-  function isLetterInQuote(letter) {
-    //let $blanks = $("#quote .blank");
-    let $blanks = $("#quote .blankLetter");
-    for (let blank of $blanks) {
-      if (letter == $(blank).attr("value").toLowerCase()) return true;
-    }
-    //let $letters = $blanks.each().attr("value");
-    //console.log("blanks", $blanks);
-    //console.log("letters", $letters);
-    return false;
-  }*/
 }
 function pressedLetter(event) {
   let letter = event.target.innerText;
@@ -132,7 +181,7 @@ function outputMessage(message, effect, color) {
   $messageArea.text(message);
   switch (effect) {
     case "flash":
-      let originalColor = $messageArea.css("background-color");
+      let originalColor = $("#messageSection").css("background-color");
       $messageArea.animate({ "background-color": color });
       $messageArea.animate({ "background-color": originalColor });
       break;
@@ -143,12 +192,12 @@ function outputMessage(message, effect, color) {
 }
 function revealLettersInQuote(letter) {
   let $blankLetters = $("#quote .blankLetter");
-  console.log("blank letters", $blankLetters);
+  //console.log("blank letters", $blankLetters);
   let isLetterFound = false;
   for (let blankLetter of $blankLetters) {
     if (isMatch(blankLetter, letter)) {
       revealLetter(blankLetter);
-      doRevealAnimation(blankLetter);
+      revealAnimation(blankLetter);
       removeBlankStatus(blankLetter);
       isLetterFound = true;
     }
@@ -171,7 +220,7 @@ function revealLettersInQuote(letter) {
     $(blankElement).removeAttr("value");
     $(blankElement).removeClass("blankLetter");
   }
-  function doRevealAnimation(blankElement) {
+  function revealAnimation(blankElement) {
     let reveal = { backgroundColor: "rgba(0, 0, 0, 0)", color: "black" };
     $(blankElement).animate(reveal);
   }
